6dea85ae54b7fe38d13c4b52deb1afbeb6754975
Hack Reactor Student <student@hackreactor.com>
2019-11-19 19:26:40 -0800

    working file upload with json

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const port = 3000
+const path = require('path');
+const controller = require('./controller.js');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.urlencoded());
+app.use(express.json());
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.post('/submit', (req , res) => {
+  controller.submit(req, res);
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
